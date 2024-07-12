1e2fd1aef32d2d822ada1b81b55f7facfdb2126c
akiosoul <161982099+akiosoul@users.noreply.github.com>
2024-07-12 05:39:02 +0000

    feat: implemented add, multiply, and divide functions

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -4,7 +4,23 @@ public class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   public double calculate(double a, double b, double c, double d, double e) {
     // Implement your code here to return the correct value.
-    return 0d;
+    double addition = add(a, b);
+    double multiplication = multiply(addition, c);
+    double power = pow(d, e);
+    double division = divide(multiplication, power);
+    return division;
+  }
+
+  public double add(double a, double b) {
+    return a + b;
+  }
+
+  public double multiply(double a, double b) {
+    return a * b;
+  }
+
+  public double divide(double a, double b) {
+    return a / b;
   }
 
   public double pow(double base, double exponent) {
